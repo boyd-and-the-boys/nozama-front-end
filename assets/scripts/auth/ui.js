a050e648065ec3d7dc21a9c2fc63083eaf0c892e
jenboyd <jboyd6290@gmail.com>
2016-10-15 19:08:13 -0400

    Update Nav Bar
    Add Log in page and User page.
    
    Update layout of nav bar.
    
    Add show / hide for nav buttons where appropriate.
    
    Remove sign in and sign up modals. Create them as inputs on the log
    in page.

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,24 +1,30 @@
 'use strict';
 
 const app = require ('../app');
+const loadProducts = require ('../../templates/products.handlebars');
+
 
 const signUpSuccess = (data) => {
   app.user = data.user;
   console.log(data);
-  $('#signUpModal').modal('hide');
-
 
 };
 
 const signInSuccess = (data) => {
   app.user = data.user;
   console.log(data);
-  $('#signInModal').modal('hide');
-
+  $('#log-in-page').hide();
+  $('#content').html(loadProducts(data));
+  $('.log-in-button').hide();
+  $('.log-out-button').show();
+  $('.change-pwd-button').show();
 };
 
 const signOutSuccess = () => {
   app.user = null;
+  $('.log-in-button').show();
+  $('.log-out-button').hide();
+  $('.change-pwd-button').hide();
 };
 
 const changePasswordSuccess = () => {
