de2e8f0abf767abbf6e3e8bcf1c81e0eae59c05a
jenboyd <jboyd6290@gmail.com>
2016-10-19 23:54:44 -0400

    Deploy to gh-pages

--- assets/scripts/products/events.js
+++ assets/scripts/products/events.js
@@ -9,14 +9,21 @@ const onGetProducts = function () {
     .fail (ui.failure);
 };
 
+const onBackToProducts = function (event) {
+  event.preventDefault();
+  $('#about-page').hide();
+  $('#content').show();
+};
+
 const onClickProduct = function (event) {
   event.preventDefault();
-
   $(event.target).parent().find('.photo-modal').first().modal('show');
 };
 
 const addHandlers = () => {
   $('#content').on('click', '.photo-small', onClickProduct);
+  $('#about-page').on('click', '#back-products-button', onBackToProducts);
+
 };
 
 module.exports = {
